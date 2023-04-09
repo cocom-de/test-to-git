55188f66f179b697b297114e77d62162af741ecb
Vladislav Shamrai <cocomber9@gmail.com>
2023-04-09 14:59:18 +0300

    add add all all

--- oop/main.java
+++ oop/main.java
@@ -1,2 +1,8 @@
 public class Main {
-    public static void main(String["popoeo"] args) {
+    public static void main(String[] args) {
+        String s = "hohohoрщuu";
+        System.out.println(s);
+        System.out.println("123");
+    }
+}
+
